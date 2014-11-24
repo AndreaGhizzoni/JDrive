49cce154d0d0e9860106f2f9649ee72bcc525b03
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-11-24 12:04:55 +0100

    solved some bug

--- src/exp/java/it/hackcaffebabe/jdrive/TestWatchService.java
+++ src/exp/java/it/hackcaffebabe/jdrive/TestWatchService.java
@@ -17,18 +17,40 @@ public class TestWatchService {
             final WatchEvent.Kind[] mod = {ENTRY_CREATE,
                                            ENTRY_DELETE,
                                            ENTRY_MODIFY };
-            Path dir = new File("/home/andrea/test").toPath();
+            final Path dir = new File("/home/andrea/test").toPath();
             dir.register( watcher, mod );
+            System.out.println("--- Starting watch on path "+dir);
 
             final SimpleFileVisitor<Path> fileVisitor = new SimpleFileVisitor<Path>() {
                 @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                dir.register(watcher, mod);
-                return FileVisitResult.CONTINUE;
+                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attr) throws IOException {
+                    dir.register(watcher, mod);
+                    return FileVisitResult.CONTINUE;
                 }
             };
 
-            WatchKey key ;
+            final SimpleFileVisitor<Path> registerExistingFiles = new SimpleFileVisitor<Path>() {
+                @Override
+                public FileVisitResult postVisitDirectory(Path d, IOException e) throws IOException {
+                    if(d.equals(dir))
+                        return FileVisitResult.CONTINUE;
+                    d.register(watcher, mod);
+                    System.out.println(d);
+                    return FileVisitResult.CONTINUE;
+                }
+
+                @Override
+                public FileVisitResult visitFile( Path file, BasicFileAttributes attr) throws IOException {
+                    System.out.println(file);
+                    return FileVisitResult.CONTINUE;
+                }
+            };
+
+            System.out.println("--- Existing files added to watcher services:");
+            Files.walkFileTree(dir, registerExistingFiles);
+            System.out.println("---");
+
+            WatchKey key;
             while (true) {
                 key = watcher.take();
 
@@ -38,34 +60,33 @@ public class TestWatchService {
                     // This key is registered only for ENTRY_CREATE events,
                     // but an OVERFLOW event can occur regardless if events
                     // are lost or discarded.
-                    if (kind == OVERFLOW) {
+                    if(kind == OVERFLOW) {
                         continue;
                     }
 
                     WatchEvent<Path> ev = (WatchEvent<Path>) event;
                     Path child = ((Path)key.watchable()).resolve(ev.context());
-                    System.out.println( String.format("event from: %s", child ));
+                    System.out.println( String.format("event from: %s", child) );
+
+                    //this doesn't work
+                    if(kind == ENTRY_DELETE) {
+                        key.reset();
+                        System.out.println("unregister service");
+                        continue;
+                    }
 
                     Boolean isFolder = child.toFile().isDirectory();
-                    if( isFolder ){
-                        if( kind == ENTRY_DELETE ){
-                            key.reset();
-                            key.cancel();
-                            watcher.close();
-                        }else {
-                            Files.walkFileTree(child, fileVisitor);
-                        }
+                    if( isFolder ) {
+                        Files.walkFileTree(child, fileVisitor);
                         //process directory
-                    } else {
-                        System.out.println(String.format("=== key: %s - path: %s", kind, child));
                     }
+                    System.out.println(String.format("=== key: %s - path: %s", kind, child));
                 }
 
                 // Reset the key -- this step is critical if you want to
                 // receive further watch events.  If the key is no longer valid,
                 // the directory is inaccessible so exit the loop.
                 if( !key.reset() ){
-                    key.cancel();
                     watcher.close();
                     break;
                 }
