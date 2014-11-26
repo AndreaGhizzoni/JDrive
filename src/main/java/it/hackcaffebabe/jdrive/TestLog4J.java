82ea473e967164921fe58eb8729410b7ddf1c62e
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-11-26 17:39:16 +0100

    added more examples for log4j2

--- src/main/java/it/hackcaffebabe/jdrive/TestLog4J.java
+++ src/main/java/it/hackcaffebabe/jdrive/TestLog4J.java
@@ -1,5 +1,6 @@
 package it.hackcaffebabe.jdrive;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -8,10 +9,23 @@ public class TestLog4J
     static final Logger logger = LogManager.getLogger(TestLog4J.class.getName());
 
     public boolean doIt( int a) {
-        logger.entry(a);
-        logger.error("Logger error message");
-        logger.info("asd");
-        return logger.exit(false);
+        //Trace level information, separately is to call you when you started
+        //in a method or program logic, and logger.trace ("entry") basic a meaning
+        logger.entry();
+        //The error levels of information, information output parameter is you
+        logger.error("Did it again!");
+        //Info level information
+        logger.info("I'm info information");
+        logger.debug("I'm debug information");
+        logger.warn("I'm warn information");
+        logger.fatal("I'm fatal information");
+        //This is the development of Level type of call: who is idle and call
+        //this, not necessarily oh！
+        logger.log(Level.DEBUG, "I'm debug information");
+        //And the entry () method to end the correspondence, and the
+        //logger.trace ("exit"); a meaning
+        logger.exit();
+        return false;
     }
 
     public static void main(String[] args){
