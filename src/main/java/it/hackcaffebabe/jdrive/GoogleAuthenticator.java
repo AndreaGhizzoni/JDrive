5c68240c07432f4cbc1d2a3fbc202e0063b2642e
AndreaGhizzoni <andrea.ghz@gmail.com>
2014-11-28 11:00:31 +0100

    added first version of my GoogleAuthenticator

---
+++ src/main/java/it/hackcaffebabe/jdrive/GoogleAuthenticator.java
@@ -0,0 +1,68 @@
+package it.hackcaffebabe.jdrive;
+
+import static it.hackcaffebabe.jdrive.AuthConst.*;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public final class GoogleAuthenticator
+{
+    private static GoogleAuthenticator instance;
+    public static GoogleAuthenticator getInstance(){
+        if(instance == null)
+            instance = new GoogleAuthenticator();
+        return instance;
+    }
+
+    private HttpTransport httpTransport;
+    private JsonFactory jsonFactory;
+    private GoogleAuthorizationCodeFlow googleAuthCodeFlow;
+    private Drive service;
+
+    private GoogleAuthenticator(){
+        this.buildHTTPTransportJsonFactory();
+        this.buildGoogleAuthCodeFlow();
+    }
+
+//==============================================================================
+// METHOD
+//==============================================================================
+    private void buildHTTPTransportJsonFactory(){
+        this.httpTransport = new NetHttpTransport();
+        this.jsonFactory = new JacksonFactory();
+    }
+
+    private void buildGoogleAuthCodeFlow(){
+         this.googleAuthCodeFlow = new GoogleAuthorizationCodeFlow.Builder(
+                 this.httpTransport, this.jsonFactory, CLIENT_ID, CLIENT_SECRET,
+                 Arrays.asList(DriveScopes.DRIVE)).setAccessType("online")
+                .setApprovalPrompt("auto").build();
+    }
+
+//==============================================================================
+// GETTER
+//==============================================================================
+    public String getAuthURL(){
+        return this.googleAuthCodeFlow.newAuthorizationUrl().
+                setRedirectUri(REDIRECT_URI).build();
+    }
+
+    public Drive getService(String auth) throws IOException {
+        GoogleTokenResponse t = this.googleAuthCodeFlow.newTokenRequest(auth)
+                .setRedirectUri(REDIRECT_URI).execute();
+        GoogleCredential c = new GoogleCredential().setFromTokenResponse(t);
+        this.service = new Drive.Builder(this.httpTransport, this.jsonFactory, c)
+                .build();
+        return this.service;
+    }
+
+}
---
+++ src/main/java/it/hackcaffebabe/jdrive/TestGoogleAPI.java
@@ -0,0 +1,38 @@
+package it.hackcaffebabe.jdrive;
+
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.model.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestGoogleAPI {
+    public static void main(String[] args){
+        try {
+            GoogleAuthenticator a = GoogleAuthenticator.getInstance();
+            String url = a.getAuthURL();
+
+            System.out.println("Please open the following URL in your browser then type the authorization code:");
+            System.out.println("  " + url);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String code = br.readLine();
+
+            Drive service = a.getService(code);
+
+            //Insert a file
+            File body = new File();
+            body.setTitle("My document");
+            body.setDescription("A test document");
+            body.setMimeType("text/plain");
+
+            java.io.File fileContent = new java.io.File("/home/andrea/document.txt");
+            FileContent mediaContent = new FileContent("text/plain", fileContent);
+
+            File file = service.files().insert(body, mediaContent).execute();
+            System.out.println("File ID: " + file.getId());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+}
