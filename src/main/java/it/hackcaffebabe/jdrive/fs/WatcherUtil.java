ab72c7aef255deee945d84711a4ec2e090214e6c
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-01-12 12:16:22 +0100

    add doc to WatcherUtil

--- src/main/java/it/hackcaffebabe/jdrive/fs/WatcherUtil.java
+++ src/main/java/it/hackcaffebabe/jdrive/fs/WatcherUtil.java
@@ -4,7 +4,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
- * TODO add doc
+ * Class that manage the base path of {@link it.hackcaffebabe.jdrive.fs.Watcher}
  */
 final class WatcherUtil
 {
