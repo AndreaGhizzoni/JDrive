867b5999c26e492283a8cdaa8758a6e33d9a74e0
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-02-17 11:22:19 +0100

    added empty test case for Configurator

---
+++ src/test/java/it/hackcaffebabe/jdrive/cfg/ConfiguratorTest.java
@@ -0,0 +1,14 @@
+package it.hackcaffebabe.jdrive.cfg;
+
+import org.junit.Test;
+
+/**
+ * Unit test for {@link it.hackcaffebabe.jdrive.cfg.Configurator}
+ */
+public class ConfiguratorTest
+{
+    @Test
+    public void testConfigurator(){
+
+    }
+}
