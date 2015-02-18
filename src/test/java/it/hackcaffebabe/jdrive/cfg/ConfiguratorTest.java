8909aa21d9e08a163a00afb9155f1fda0c5a77e2
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-02-18 14:26:42 +0100

    added method to build and clean working direcotry

--- src/test/java/it/hackcaffebabe/jdrive/cfg/ConfiguratorTest.java
+++ src/test/java/it/hackcaffebabe/jdrive/cfg/ConfiguratorTest.java
@@ -1,6 +1,11 @@
 package it.hackcaffebabe.jdrive.cfg;
 
+import it.hackcaffebabe.jdrive.Paths;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Unit test for {@link it.hackcaffebabe.jdrive.cfg.Configurator}
@@ -9,6 +14,29 @@ public class ConfiguratorTest
 {
     @Test
     public void testConfigurator(){
+        buildWD();
 
+        cleanWD();
+    }
+
+//==============================================================================
+//  TEST CASE UTIL METHOD
+//==============================================================================
+    // create a method to build working directory
+    public void buildWD(){
+        try {
+            Paths.buildWorkingDirectory();
+        } catch (IOException e) {
+            Assert.fail("Fail to build wd.");
+        }
+    }
+
+    // create a method to clean the working directory
+    public void cleanWD(){
+        try {
+            Files.delete(java.nio.file.Paths.get(Paths.PATH_APP));
+        } catch (IOException e) {
+            Assert.fail("Fail to clean wd.");
+        }
     }
 }
