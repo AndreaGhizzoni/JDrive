f49ba2f9e0d02964b1712d589a48962b1dbf964a
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-09-23 13:17:19 +0200

    added file walker

--- src/main/java/it/hackcaffebabe/jdrive/fs/Syncer.java
+++ src/main/java/it/hackcaffebabe/jdrive/fs/Syncer.java
@@ -1,8 +1,13 @@
 package it.hackcaffebabe.jdrive.fs;
 
+import it.hackcaffebabe.jdrive.cfg.Configurator;
+import it.hackcaffebabe.jdrive.cfg.Keys;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -32,7 +37,33 @@ public final class Syncer {
     }
 
     public void check(){
-
+        Path watchedDir = Paths.get((String) Configurator.getInstance()
+                .get(Keys.WATCHED_DIR));
+        try {
+            Files.walkFileTree(watchedDir, new ServiceAdder());
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
+//==============================================================================
+//  INNER CLASS
+//==============================================================================
+    /* inner class that walk down a given path and cache all the files */
+    private class ServiceAdder extends SimpleFileVisitor<Path> {
+//        private WatcherCache cache;
+//        public ServiceAdder(){
+//            try {
+//                cache = WatcherCache.getInstnace();
+//            } catch (IOException e) {
+//                log.error(e.getMessage(), e);
+//            }
+//        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes a)
+                throws IOException {
+            return FileVisitResult.CONTINUE;
+        }
+    }
 }
