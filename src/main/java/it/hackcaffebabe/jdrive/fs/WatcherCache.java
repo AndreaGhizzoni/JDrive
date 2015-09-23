2dfe80535edd97b0fb92523d02979c3b617e2d84
AndreaGhizzoni <andrea.ghz@gmail.com>
2015-09-23 13:36:48 +0200

    importing Gson library and used in WatcherCache

--- src/main/java/it/hackcaffebabe/jdrive/fs/WatcherCache.java
+++ src/main/java/it/hackcaffebabe/jdrive/fs/WatcherCache.java
@@ -1,12 +1,13 @@
 package it.hackcaffebabe.jdrive.fs;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import it.hackcaffebabe.jdrive.cfg.Default;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -36,6 +37,28 @@ class WatcherCache {
         if( !cacheFile.toFile().exists() ) {
             Files.createFile(cacheFile);
             log.debug("Cache file created successfully.");
+        }else{
+            loadCache();
+        }
+    }
+
+    private void loadCache() throws IOException{
+        BufferedReader br = new BufferedReader( new FileReader(cacheFile.toFile()) );
+        StringBuilder b = new StringBuilder();
+
+        boolean finish = false; String line;
+        while( !finish ){
+            line = br.readLine();
+            if( line == null )
+                finish = true;
+            else
+                b.append(line);
+        }
+
+        String json = b.toString();
+        if( !json.isEmpty() ) {
+            Type type = new TypeToken<Map<String, String>>(){}.getType();
+            this.cache = new Gson().fromJson(json, type);
         }
     }
 
@@ -71,19 +94,21 @@ class WatcherCache {
 
     @Override
     public String toString(){
-        StringBuilder b = new StringBuilder();
-        b.append("{").append("\n");
-        String p; String l; int c = 1;
-        for( Map.Entry<Path, Long> e: this.cache.entrySet() ){
-            p = e.getKey().toFile().getAbsoluteFile().toString();
-            l = e.getValue().toString();
-            b.append(" \"").append(p).append("\": \"").append(l).append("\"");
-
-            if( c++ != this.cache.size() )
-                b.append(",");
-            b.append("\n");
-        }
-        b.append("}");
-        return b.toString();
+        Gson g = new Gson();
+        return g.toJson(this.cache);
+//        StringBuilder b = new StringBuilder();
+//        b.append("{").append("\n");
+//        String p; String l; int c = 1;
+//        for( Map.Entry<Path, Long> e: this.cache.entrySet() ){
+//            p = e.getKey().toFile().getAbsoluteFile().toString();
+//            l = e.getValue().toString();
+//            b.append(" \"").append(p).append("\": \"").append(l).append("\"");
+//
+//            if( c++ != this.cache.size() )
+//                b.append(",");
+//            b.append("\n");
+//        }
+//        b.append("}");
+//        return b.toString();
     }
 }
