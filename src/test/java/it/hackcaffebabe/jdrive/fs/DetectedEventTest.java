aaf03ef2250ef15637eaaec1617de35c888767b5
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-03-15 09:31:20 +0100

    added test case for DetectedEvent

--- src/test/java/it/hackcaffebabe/jdrive/fs/DetectedEventTest.java
+++ src/test/java/it/hackcaffebabe/jdrive/fs/DetectedEventTest.java
@@ -2,79 +2,109 @@ package it.hackcaffebabe.jdrive.fs;
 
 import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardWatchEventKinds;
-import java.nio.file.WatchEvent;
-import java.util.Date;
+import static java.nio.file.StandardWatchEventKinds.*;
 
 /**
  * Test for class {@link DetectedEvent}
  */
 public class DetectedEventTest {
+    public final Path testPath = Paths.get(System.getProperty("java.io.tmpdir"));
 
     @Test
-    public void testDetectedObject(){
-        DetectedEvent d = new DetectedEvent();
+    public void testPassingWrongArguments(){
+        // test getKind()
+        DetectedEvent d = new DetectedEvent( null, testPath, "some msg" );
+        Assert.assertNull("Expected null from passing null as kind", d.getKind());
+        Assert.assertTrue(
+                "", d.containError());
 
-        // test watch event kind
-        WatchEvent.Kind k;
-        d.setKind( null );
-        Assert.assertEquals("Expecting null from getKind() if setKind(null)",
-                null, d.getKind());
+        // test getFile() & getLastModify()
+        d = new DetectedEvent( ENTRY_CREATE, null, "some msg" );
+        Assert.assertNull(
+                "Expected null from passing null as file path",
+                d.getFile()
+        );
+        Assert.assertTrue(
+                "Expected negative number as last modify from null file path",
+                d.getLastModify() < 0L
+        );
 
-        d.setKind( StandardWatchEventKinds.OVERFLOW );
-        Assert.assertEquals("Expecting null from getKind() if setKind(OVERFLOW)",
-                null, d.getKind());
+        // test getMessage()
+        d = new DetectedEvent( ENTRY_CREATE, testPath, null );
+        Assert.assertNotNull(
+                "Expected not null string from given null message",
+                d.getMessage()
+        );
 
-        k = StandardWatchEventKinds.ENTRY_CREATE;
-        d.setKind( k );
-        Assert.assertEquals("Expecting kind ENTRY_CREATE from getKind() if " +
-                        "setKind(ENTRY_CREATE)", k, d.getKind());
+        // test wrong constructor for errors
+        DetectedEvent e5 = new DetectedEvent( null, null );
+        Assert.assertNotNull(
+                "Expected default error message from using wrong constructor for errors",
+                e5.getMessage()
+        );
+    }
 
-        k = StandardWatchEventKinds.ENTRY_MODIFY;
-        d.setKind( k );
-        Assert.assertEquals("Expecting kind ENTRY_MODIFY from getKind() if " +
-                        "setKind(ENTRY_MODIFY)", k, d.getKind());
+    @Test
+    public void testDetectedEventStatus(){
+        boolean mustBeTrue = new DetectedEvent().isEmpty();
+        Assert.assertTrue("Expecting empty DetectedEvent.", mustBeTrue);
 
-        k = StandardWatchEventKinds.ENTRY_DELETE;
-        d.setKind( k );
-        Assert.assertEquals("Expecting kind ENTRY_DELETE from getKind() if " +
-                        "setKind(ENTRY_DELETE)", k, d.getKind());
+        // test creation event
+        DetectedEvent creation = new DetectedEvent(
+                ENTRY_CREATE,
+                testPath,
+                "This is a creation event of test path"
+        );
+        boolean isCreation = creation.isEventCreate();
+        Assert.assertTrue(
+                "Expecting true from isEventCreate() from a Creation event.",
+                isCreation
+        );
 
-        // test file path
-        String s; Path p; File f;
-        d.setDetectedObject("");
-        Assert.assertTrue("Expecting null from getPath() if setDetectedObject(\"\")",
-                d.getPath() == null);
+        // test modification event
+        DetectedEvent modification = new DetectedEvent(
+                ENTRY_MODIFY,
+                testPath,
+                "This is a modification event of test path"
+        );
+        boolean isModification = modification.isEventModify();
+        Assert.assertTrue(
+                "Expecting true from isEventModify() from a Modification event.",
+                isModification
+        );
 
-        s = "/tmp";
-        d.setDetectedObject(s);
-        Assert.assertEquals("Expecting /tmp from getPathAsString() if setDetectedObject(\"/tmp\")",
-                s, d.getPathAsString());
+        // test delete event
+        DetectedEvent delete = new DetectedEvent(
+                ENTRY_DELETE,
+                testPath,
+                "This is a Delete event of test path"
+        );
+        boolean isDelete = delete.isEventDelete();
+        Assert.assertTrue(
+                "Expecting true from isEventDelete() from a Delete event.",
+                isDelete
+        );
 
-        p = Paths.get(s);
-        d.setDetectedObject(p);
-        Assert.assertEquals("Expecting /tmp from getPath() if setDetectedObject(\"/tmp\")",
-                p, d.getPath() );
-
-        f = new File(s);
-        d.setDetectedObject(f);
-        Assert.assertEquals("Expecting /tmp from getFile() if setDetectedObject(\"/tmp\")",
-                f, d.getFile() );
-
-        // test last modify
-        long l = 0L;
-        d.setLastModify(l);
-        Assert.assertTrue("Expecting -1L from getLastModify() if setLastModify(0L)",
-                d.getLastModify() == -1L);
-
-        l = new Date().getTime();
-        d.setLastModify(l);
-        Assert.assertTrue("Expecting the long did I set from getLastModify()",
-                d.getLastModify() == l);
-
+        // test error event
+        DetectedEvent e1 = new DetectedEvent(
+                null, null, "This is a error event"
+        );
+        DetectedEvent e2 = new DetectedEvent(
+                null, null, null
+        );
+        DetectedEvent e3 = new DetectedEvent(
+                null, testPath, "This is a error event"
+        );
+        DetectedEvent e4 = new DetectedEvent(
+                null, testPath, null
+        );
+        boolean isError = e1.containError() && e2.containError() &&
+                          e3.containError() && e4.containError();
+        Assert.assertTrue(
+                "Expecting true from containsError() from a error event.",
+                isError
+        );
     }
 }
