4de392fe8691ecacc7824d5a8255237ae10f6b80
Andrea Ghizzoni <andrea.ghz@gmail.com>
2016-04-21 15:01:23 +0200

    added first implementation of new Google Authentication API

---
+++ src/main/java/it/hackcaffebabe/jdrive/auth/google/GoogleAuthenticatorV2.java
@@ -0,0 +1,131 @@
+package it.hackcaffebabe.jdrive.auth.google;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.Drive;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * // Build a new authorized API client service.
+ * Drive service = getDriveService();
+ *
+ */
+public final class GoogleAuthenticatorV2 {
+
+    private static GoogleAuthenticatorV2 instance;
+    private static final Logger log = LogManager.getLogger(
+            GoogleAuthenticatorV2.class.getSimpleName()
+    );
+
+    /**
+     * TODO add doc
+     * @return
+     */
+    public static GoogleAuthenticatorV2 getInstance() throws GeneralSecurityException,
+            IOException {
+        if( instance == null )
+            instance = new GoogleAuthenticatorV2();
+        return instance;
+    }
+
+    /* Global instance of the {@link FileDataStoreFactory}. */
+    private  FileDataStoreFactory DATA_STORE_FACTORY;
+    /* Global instance of the JSON factory. */
+    private JsonFactory JSON_FACTORY;
+    /* Global instance of the HTTP transport. */
+    private HttpTransport HTTP_TRANSPORT;
+
+    private GoogleAuthorizationCodeFlow CODE_FLAW;
+
+    /* Global instance of the scopes */
+    private static final List<String> SCOPES = Arrays.asList(
+            DriveScopes.DRIVE_METADATA_READONLY
+    );
+
+    /* constructor */
+    private GoogleAuthenticatorV2() throws GeneralSecurityException, IOException {
+        log.entry();
+        this.buildHTTPTransportJsonFactory();
+        this.buildDataStore();
+        log.debug("GoogleAuthenticator created correctly.");
+    }
+
+//==============================================================================
+//  METHOD
+//==============================================================================
+    /* build up the HTTPTransport and JsonFactory */
+    private void buildHTTPTransportJsonFactory() throws GeneralSecurityException,
+            IOException {
+        this.HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        this.JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    }
+
+    /* build up the data store and load stored credential if the are any */
+    private void buildDataStore() throws IOException{
+        DATA_STORE_FACTORY = new FileDataStoreFactory(
+                AuthenticationConst.DATA_STORE_DIR
+        );
+    }
+
+    /* build up the Google Authentication Flow */
+    private void buildGoogleAuthCodeFlow() throws IOException {
+        // Load client secrets.
+        InputStream in = GoogleAuthenticatorV2.class.getResourceAsStream(
+                "/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                JSON_FACTORY, new InputStreamReader(in)
+        );
+
+        // Build flow and trigger user authorization request.
+        CODE_FLAW = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+            .setDataStoreFactory(DATA_STORE_FACTORY)
+            .setAccessType("offline")
+            .build();
+    }
+
+    /*
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    private Credential authorize() throws IOException {
+        return new AuthorizationCodeInstalledApp(
+                CODE_FLAW,
+                new LocalServerReceiver()
+        ).authorize("user");
+    }
+
+    /**
+     * Build and return an authorized Drive client service.
+     * @return an authorized Drive client service
+     * @throws IOException
+     */
+    public Drive getDriveService() throws IOException {
+        Credential credential = authorize();
+        return new Drive.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(AuthenticationConst.APP_NAME)
+                .build();
+    }
+}
+
+
