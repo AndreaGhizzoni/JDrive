6b5b57e376766173d747019ab7d16c39b15a6576
AndreaGhizzoni <andrea.ghz@gmail.com>
2016-09-20 14:05:20 +0200

    done more upload tests

--- src/main/java/it/hackcaffebabe/jdrive/TestIOGDrive.java
+++ src/main/java/it/hackcaffebabe/jdrive/TestIOGDrive.java
@@ -16,6 +16,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -44,9 +45,28 @@ public class TestIOGDrive {
 //        String id = createEmptyFile(d);
 //        log.info("File ID: " + id);
 
-        File insertedFile = uploadFile(d);
-        log.info("id file: "+insertedFile.getId());
-        downloadRemoteFile(d, insertedFile.getId());
+//        Path localFilePath = Paths.get("/home/andrea/Google Drive/test.txt");
+//        File insertedFile = uploadFile(d, localFilePath);
+//        log.info("id file: "+insertedFile.getId());
+//        downloadRemoteFile(d, insertedFile.getId());
+
+        // FILEs TESTED:
+        // binary     ok
+        // zip, tar   ok
+        // pdf        ok
+        // plain text ok
+        // jpg        ok
+
+        Path uploadPath = Paths.get("/home/andrea/Google Drive/upload");
+        ArrayList<File> remoteFiles = new ArrayList<>();
+        for( java.io.File f : uploadPath.toFile().listFiles() ){
+            File remoteFile = uploadFile(d, f.toPath());
+            remoteFiles.add(remoteFile);
+        }
+
+        for(File remoteFile : remoteFiles ){
+            downloadRemoteFile(d,remoteFile);
+        }
     }
 
     // NB: invoke this method multiple times will always create a NEW file
@@ -67,14 +87,15 @@ public class TestIOGDrive {
         return file.getId();
     }
 
-    private static void downloadRemoteFile(Drive driveService, String fileId){
+    private static void downloadRemoteFile(Drive driveService, File file){
         try {
             log.info("create output stream to local file...");
             OutputStream outputStream = new FileOutputStream(
-                    Paths.get("/home/andrea/Google Drive/downloaded/"+fileId).toFile()
+                    Paths.get("/home/andrea/Google Drive/downloaded/"+file.getName())
+                            .toFile()
             );
-            log.info("try to download file: "+fileId);
-            driveService.files().get(fileId)
+            log.info("try to download file: "+file.getName());
+            driveService.files().get(file.getId())
                     .executeMediaAndDownloadTo(outputStream);
             log.info("download ok.");
         } catch (Exception e) {
@@ -83,15 +104,14 @@ public class TestIOGDrive {
     }
 
     // from here https://goo.gl/zxWV9n
-    private static File uploadFile(Drive service) {
-        Path localFilePath = Paths.get("/home/andrea/Google Drive/test.txt");
+    private static File uploadFile(Drive service, Path localFilePath) {
         java.io.File localFile = localFilePath.toFile();
 
         // File's metadata.
         File body = new File()
-                .setName("test.txt")
-                .setDescription("description")
-                .setModifiedByMeTime(new DateTime(new Date(localFile.lastModified())));
+                .setName(localFile.getName())
+                .setDescription("description");
+//                .setModifiedByMeTime(new DateTime(new Date(localFile.lastModified())));
         //body.setMimeType();
 
         // Set the parent folder.
@@ -101,14 +121,15 @@ public class TestIOGDrive {
 //        }
 
         try {
-            log.info("Try to make input stream content...");
+            log.info("try to make input stream content...");
             InputStreamContent inputStreamContent = new InputStreamContent(
                     null,
                     new FileInputStream( localFile )
             );
 
-            log.info("Try to upload...");
-            return service.files().create(body, inputStreamContent).execute();
+            log.info("try to upload "+localFile.getAbsolutePath());
+            return service.files().create(body, inputStreamContent)
+                    .execute();
         } catch (IOException e) {
             fatal(e.getMessage(), e);
         }
