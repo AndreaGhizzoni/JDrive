7c346a9595e1328a276c51001244bd2dac2c02d9
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-06-08 12:03:17 +0200

    refactoring comments and removed unused import

--- src/main/java/it/hackcaffebabe/jdrive/auth/google/AuthenticationConst.java
+++ src/main/java/it/hackcaffebabe/jdrive/auth/google/AuthenticationConst.java
@@ -2,18 +2,17 @@ package it.hackcaffebabe.jdrive.auth.google;
 
 import com.google.api.services.drive.DriveScopes;
 
-import java.io.File;
 import java.util.Arrays;
 import java.util.List;
 
 final class AuthenticationConst
 {
-    /** the application name */
+    /** The application name used in console.developers.google.com */
     static final String APP_NAME = "JDrive";
 
-    /** list of Scopes */
+    /** List of scopes API */
     static final List<String> SCOPES = Arrays.asList(
-            DriveScopes.DRIVE,
-            DriveScopes.DRIVE_FILE
+        DriveScopes.DRIVE,
+        DriveScopes.DRIVE_FILE
     );
 }
