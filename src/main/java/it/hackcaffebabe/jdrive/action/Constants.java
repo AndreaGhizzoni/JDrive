20bce49c7bb6f1fc404ded2332f6c93ac71bf831
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-06-21 15:44:50 +0200

    made action.Constants a package level class

--- src/main/java/it/hackcaffebabe/jdrive/action/Constants.java
+++ src/main/java/it/hackcaffebabe/jdrive/action/Constants.java
@@ -6,13 +6,13 @@ import java.net.UnknownHostException;
 /**
  * TODO add doc
  */
-public final class Constants
+final class Constants
 {
     /** default action port */
-    public static final int SERVER_PORT = 12345;
+    static final int SERVER_PORT = 12345;
 
     /** return {@link java.net.InetAddress} of localhost */
-    public static InetAddress getLocalhost(){
+    static InetAddress getLocalhost(){
         InetAddress localhost = null;
         try {
             localhost = InetAddress.getByName("localhost");
