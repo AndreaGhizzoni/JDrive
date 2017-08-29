584ebaadfd1917c23726779506b622108540c4b4
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-08-29 10:12:01 +0200

    added MIMEType class for constants

---
+++ src/main/java/it/hackcaffebabe/jdrive/remote/google/MIMEType.java
@@ -0,0 +1,19 @@
+package it.hackcaffebabe.jdrive.remote.google;
+
+import java.util.HashMap;
+
+public class MIMEType
+{
+    public static final String SPREADSHEET = "application/vnd.google-apps.spreadsheet";
+    public static final String DOCUMENT = "application/vnd.google-apps.document";
+    public static final String DRAWING = "application/vnd.google-apps.drawing";
+    public static final String PRESENTATION = "application/vnd.google-apps.presentation";
+    public static final String FOLDER = "application/vnd.google-apps.folder";
+
+    public static HashMap<String, String> Conversion = new HashMap<String, String>() {{
+        put( DOCUMENT, "application/pdf" );
+        put( SPREADSHEET, "application/pdf" );
+        put( DRAWING, "image/png" );
+        put( PRESENTATION, "application/pdf" );
+    }};
+}
