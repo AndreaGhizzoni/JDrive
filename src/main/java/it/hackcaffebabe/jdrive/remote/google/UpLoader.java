e03c17616fcf857b66c25596df64aa56df51ab7a
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-08-29 11:48:24 +0200

    removed commented code

--- src/main/java/it/hackcaffebabe/jdrive/remote/google/UpLoader.java
+++ src/main/java/it/hackcaffebabe/jdrive/remote/google/UpLoader.java
@@ -41,24 +41,13 @@ public class UpLoader  implements Runnable
                 detectedEvent = queueFromWatcher.take();
                 if (detectedEvent instanceof Create) {
                     log.debug(((Create) detectedEvent).toString());
+
                     File uploaded = driveFileManager.uploadFile(
                             detectedEvent.getFile()
                     );
                     Testing_API.logFile(uploaded);
-//                    localToRemoteFileCombination.put(
-//                        detectedEvent.getFile(),
-//                        uploaded.getId()
-//                    );
                 } else if (detectedEvent instanceof Modify) {
                     log.debug(((Modify) detectedEvent).toString());
-//                    String remoteFileId = localToRemoteFileCombination.get(
-//                        detectedEvent.getFile()
-//                    );
-//                    if( remoteFileId == null || remoteFileId.isEmpty() ){
-//                        throw new NoSuchElementException(
-//                            "No remote File associated with "+detectedEvent.getFile().toAbsolutePath()
-//                        );
-//                    }
 
                     File updatedRemoteFile = driveFileManager.updateRemoteContent(
                         detectedEvent.getFile()
@@ -66,15 +55,7 @@ public class UpLoader  implements Runnable
                     Testing_API.logFile(updatedRemoteFile);
                 } else if (detectedEvent instanceof Delete) {
                     log.debug(((Delete) detectedEvent).toString());
-//                    String remoteFileId = localToRemoteFileCombination.get(
-//                            detectedEvent.getFile()
-//                    );
-//                    if (remoteFileId == null || remoteFileId.isEmpty()) {
-//                        throw new NoSuchElementException(
-//                                "No remote File associated with " + detectedEvent.getFile().toAbsolutePath()
-//                        );
-//                    }
-//
+
                     driveFileManager.deleteRemoteFileFrom( detectedEvent.getFile() );
                 } else if (detectedEvent instanceof Error) {
                     log.debug(((Error) detectedEvent).toString());
