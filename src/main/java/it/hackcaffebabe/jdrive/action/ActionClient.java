c10749066462f6fffcd7dfa58a26048d077abaa0
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-09-01 11:35:54 +0200

    added heavy debug log to address some problems under win10 with firewall, defender and anti-virus crap active

--- src/main/java/it/hackcaffebabe/jdrive/action/ActionClient.java
+++ src/main/java/it/hackcaffebabe/jdrive/action/ActionClient.java
@@ -1,5 +1,8 @@
 package it.hackcaffebabe.jdrive.action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.*;
 import java.net.Socket;
 
@@ -11,6 +14,8 @@ import static it.hackcaffebabe.jdrive.action.Constants.getLocalhost;
  */
 public class ActionClient
 {
+    private static final Logger log = LogManager.getLogger();
+
     /** Send Message.QUIT to action server. */
     public static void sendQuitRequest() throws IOException {
         send( Message.QUIT );
@@ -22,24 +27,33 @@ public class ActionClient
     }
 
     private static String send( String message ) throws IOException {
+        log.debug("Try to connect to: "+getLocalhost()+":"+SERVER_PORT);
         Socket socket = new Socket( getLocalhost(), SERVER_PORT );
+        log.debug("Connection established");
         BufferedWriter outToServer = new BufferedWriter(
             new OutputStreamWriter( socket.getOutputStream() )
         );
         BufferedReader inFromServer = new BufferedReader(
             new InputStreamReader( socket.getInputStream() )
         );
+        log.debug("Streams obtained");
 
+        log.debug("Try to send: "+message);
         outToServer.append( message ).append( "\n" );
         outToServer.flush();
+        log.debug("Message sent.");
 
+        log.debug("Waiting for server response...");
         String response;
         while( (response = inFromServer.readLine()) != null ) {
             break;
         }
+        log.debug("Server response obtained: "+response);
 
+        log.debug("Closing streams");
         outToServer.close();
         inFromServer.close();
+        log.debug("Closing socket");
         socket.close();
         return response;
     }
