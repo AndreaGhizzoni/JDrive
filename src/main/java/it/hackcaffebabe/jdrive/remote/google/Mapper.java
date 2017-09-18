621324fa930b9b9eed46df8f1167e59ad21ba9be
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-09-18 12:14:38 +0200

    now remove methods has return type. refactoring come log

--- src/main/java/it/hackcaffebabe/jdrive/remote/google/Mapper.java
+++ src/main/java/it/hackcaffebabe/jdrive/remote/google/Mapper.java
@@ -7,10 +7,13 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * TODO add doc
@@ -56,8 +59,16 @@ public class Mapper
     }
 
     public synchronized void put( String path, boolean accessible, Optional<File> remote ){
-        localToRemote.put( new AccessiblePath(path, accessible), remote );
-        logEntry("Put", path, accessible, remote);
+        AccessiblePath accessiblePath = new AccessiblePath( path, accessible );
+        logEntry("Try to Put", accessiblePath, remote);
+        Optional<File> previousFile = localToRemote.put( accessiblePath, remote );
+        if( previousFile == null ) {
+            log.debug("Put ok.");
+        }else{
+            log.debug(String.format(
+                "Put overwritten previous value: %s", previousFile.orElse(null)
+            ));
+        }
     }
 
     public synchronized File get( Path path ) {
@@ -65,23 +76,39 @@ public class Mapper
     }
 
     public synchronized File get( String path ) {
+        log.debug("Try to get remote file associated to path="+path);
         AccessiblePath accessiblePath = new AccessiblePath( path );
         Optional<File> optFile = localToRemote.get( accessiblePath );
-
-        logEntry("Get", accessiblePath, optFile );
-        return optFile == null ? null : optFile.orElse(null);
+        if( optFile == null ) {
+            log.debug(String.format("Remote file from %s not found", path));
+            return null;
+        }else {
+            logEntry("Get ok", accessiblePath, optFile);
+            return optFile.orElse( null );
+        }
     }
 
     public synchronized String lookup( File remote ){
+        log.debug(String.format(
+            "Try to lookup path associated with remote file: %s",
+            remote == null ? "null" : remote.getName()
+        ));
         Optional<AccessiblePath> optAccessiblePath = look( remote );
 
-        logEntry(
-            "Lookup",
-            optAccessiblePath.map( accPath -> accPath.getPath() ).orElse("null"),
-            optAccessiblePath.map( accPath -> accPath.isAccessible() ).orElse(false),
-            Optional.ofNullable(remote)
-        );
-        return optAccessiblePath.isPresent() ? optAccessiblePath.get().getPath() : null;
+        if( optAccessiblePath.isPresent() ){
+            logEntry(
+                "Lookup",
+                optAccessiblePath.map( accPath -> accPath.getPath() ).orElse("null"),
+                optAccessiblePath.map( accPath -> accPath.isAccessible() ).orElse(false),
+                Optional.ofNullable(remote)
+            );
+            return optAccessiblePath.get().getPath();
+        }else{
+            log.debug(String.format(
+                "Path associated with %s not found", remote.getName()
+            ));
+            return null;
+        }
     }
 
     private synchronized Optional<AccessiblePath> look( File remote ){
@@ -95,23 +122,37 @@ public class Mapper
             .findAny();
     }
 
-    public synchronized void remove( String path ) {
-        Optional<File> remoteFile = localToRemote.remove(
+    public synchronized File remove( String path ) {
+        log.debug("Try to remove remote file associated with path: "+path);
+        Optional<File> optRemoteFile = localToRemote.remove(
             new AccessiblePath( path )
         );
-        logEntry("Removed", path, true, remoteFile);
+
+        if( optRemoteFile == null ){
+            log.debug("No associated remote file found wuth path: "+path);
+            return null;
+        }else{
+            logEntry("Removed", path, true, optRemoteFile);
+            return optRemoteFile.orElse(null);
+        }
     }
 
-    public synchronized void remove( File remoteFile ) {
+    public synchronized String remove( File remoteFile ) {
+        log.debug(String.format(
+            "Try to remove path from remote file: %s",
+            remoteFile == null ? "null" : remoteFile.getName()
+        ));
         Optional<AccessiblePath> optAccessiblePath = look( remoteFile );
 
         if( optAccessiblePath.isPresent() ) {
             remove( optAccessiblePath.get().getPath() );
+            return optAccessiblePath.get().getPath();
         }else{
             log.debug(String.format(
-                "Noting to do: remote file %s not found with any associated key.",
-                remoteFile.getName()
+                "Noting to do: remote file %s not found with any associated path.",
+                remoteFile == null ? "null" : remoteFile.getName()
             ));
+            return null;
         }
     }
 
