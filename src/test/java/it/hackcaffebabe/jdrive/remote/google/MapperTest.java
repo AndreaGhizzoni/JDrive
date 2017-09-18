2acc77793269975b71f2521439905c80f171246e
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-09-18 11:05:43 +0200

    added unit test for Mapper

---
+++ src/test/java/it/hackcaffebabe/jdrive/remote/google/MapperTest.java
@@ -0,0 +1,50 @@
+package it.hackcaffebabe.jdrive.remote.google;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Test class for {@link it.hackcaffebabe.jdrive.remote.google.Mapper}
+ */
+public class MapperTest
+{
+    @Test
+    public void test_PutGetRemovePathOnly(){
+        List<Path> paths = new ArrayList<Path>(){{
+            add(Paths.get("/tmp/file1"));
+            add(Paths.get("/tmp"));
+            add(Paths.get("/not_a_folder"));
+        }};
+
+        Mapper mapper = Mapper.getInstance();
+        paths.forEach( path -> {
+            mapper.put( path );
+            Assert.assertTrue(
+                "Putting a single path without any restriction must be "+
+                    "accessible == true",
+                mapper.isAccessible( path )
+            );
+            Assert.assertNull(
+                "Putting a single path without any remote file, must get "+
+                    "a null object from get()",
+                mapper.get( path )
+            );
+
+            mapper.remove( path.toString() );
+            Assert.assertFalse(
+                "Removing path must resulting in accessible == false",
+                mapper.isAccessible( path )
+            );
+            Assert.assertNull(
+                "Removing path must resulting in a null object from get()",
+                mapper.get( path )
+            );
+        });
+    }
+
+}
