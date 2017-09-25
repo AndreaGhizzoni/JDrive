65cc00415b4c20d28f984710665ff38cd7307634
AndreaGhizzoni <andrea.ghz@gmail.com>
2017-09-25 15:57:55 +0200

    dumb test

--- src/main/java/it/hackcaffebabe/jdrive/TestStuff.java
+++ src/main/java/it/hackcaffebabe/jdrive/TestStuff.java
@@ -2,11 +2,47 @@ package it.hackcaffebabe.jdrive;
 
 
 public class TestStuff {
-    public static void main(String...args){
-        String p = "/home/andrea/Google Drive";
 
-        System.out.println(p);
-        String g = "Google Drive";
-            System.out.println( p.replaceFirst(g, "") );
+    private static class T {
+        private Thread rT;
+
+        public T(){
+            this.rT = new Thread( () -> {
+                while (!Thread.interrupted()) {
+                    System.out.println("alive");
+//                    try {
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+                }
+            } , "name" );
+        }
+
+        public void start(){
+            this.rT.start();
+        }
+
+        public void startClosing(){
+            System.out.println("closing");
+            if( this.rT != null )
+                this.rT.interrupt();
+        }
+    }
+
+    public static void main(String...args) {
+//        String p = "/home/andrea/Google Drive";
+//        System.out.println(p);
+//        String g = "Google Drive";
+//            System.out.println( p.replaceFirst(g, "") );
+
+        T r = new T();
+        r.start();
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        r.startClosing();
     }
 }
